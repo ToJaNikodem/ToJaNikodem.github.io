5415d4fb22294ffdc3cd6ba21bf59f05f3e8f5da
ToJaNikodem <nikodembuczkowski05@gmail.com>
2023-04-28 20:05:47 +0200

    28.04.2023

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,6 +1,25 @@
 document.getElementById("hamburger").addEventListener("click", showMenu);
 
+const menu = document.getElementById("menu");
+const hamburger = document.getElementById("hamburger");
+
 function showMenu() {
-   document.getElementById("menu").classList.toggle("active");
-   document.getElementById("hamburger").classList.toggle("active");
+	menu.classList.toggle("active");
+	hamburger.classList.toggle("active");
 }
+
+window.onclick = function (event) {
+	if (!event.target.matches("span")) {
+		if (
+			menu.classList.contains("active") &&
+			hamburger.classList.contains("active")
+		) {
+			menu.classList.remove("active");
+			hamburger.classList.remove("active");
+		}
+	}
+};
+
+document.getElementById("menu").addEventListener("click", function (event) {
+	event.stopPropagation();
+});
